00e8543bce71330d4f74769caaae03437bedce71
Worsleychase <worsleychase@gmail.com>
2025-02-11 22:57:07 +0000

    Testing thingy

--- 2-6-25/lab1Ex.py
+++ 2-6-25/lab1Ex.py
@@ -1,29 +1,34 @@
 #! /usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
 
 filename = 'NGC6341.dat'
 
-blueIn, greenIn, redIn = np.loadtxt(filename,usecols=(8,14,26),unpack=True)
+blueIn, greenIn, redIn = np.loadtxt(filename, usecols=(8, 14, 26), unpack=True)
 
-quality_cut = np.where((redIn > 20) &\
-                       (blueIn > 30) &\
+quality_cut = np.where((redIn > 20) &
+                       (blueIn > 30) &
                        (greenIn > 60))
 
-blue = np.delete(blueIn,quality_cut)
-red = np.delete(redIn,quality_cut)
-green = np.delete(greenIn,quality_cut)
+blue = np.delete(blueIn, quality_cut)
+red = np.delete(redIn, quality_cut)
+green = np.delete(greenIn, quality_cut)
 
 magnitude = blue
-color     = blue - red
+color = blue - red
 
-fig, ax = plt.subplots( figsize=(5, 7) )
+fig, ax = plt.subplots(figsize=(5, 7))
+xy = np.vstack([color, magnitude])
+z = gaussian_kde(xy)(xy)
 
-plt.plot(color, (magnitude), "k.",markersize=0.3)
-plt.gca().invert_yaxis()
-plt.title('This is my super duper title')
-plt.xlabel('Color: B-R')
-plt.ylabel('Magnitude: B')
-plt.ylim(25,14)
-plt.xlim(-2,5)
+ax.scatter(color, magnitude, c=z, s=1)
+ax.invert_yaxis()
+ax.set_title('This is my super duper title')
+ax.set_xlabel('Color: B-R')
+ax.set_ylabel('Magnitude: B')
+ax.set_ylim(25, 14)
+ax.set_xlim(-2, 5)
+
 plt.savefig('lab1.png')
+plt.show()
