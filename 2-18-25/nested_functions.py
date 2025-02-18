d74d59fcdf29cfc343e23a6b10539daa44585161
Worsleychase <worsleychase@gmail.com>
2025-02-18 23:00:23 +0000

    In class work 2-18-25

---
+++ 2-18-25/nested_functions.py
@@ -0,0 +1,17 @@
+def functionA(a, b, c):
+    value = a
+    value = value + functionB(b, c) + functionC(y = b, x = a)
+    return value
+
+#function B does not care what the arguments are called, but it does care about the order. 
+def functionB(x, y): 
+	value = x * y    
+	return value
+
+#function C does care -- this is the difference between args and kwargs
+def functionC(x = 'a', y = 'b'):
+	return x + y
+
+# Oops, we forgot to pass any arguments :(
+result = functionA(1,2,3)
+print(result)
